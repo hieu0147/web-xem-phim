eb5526918400653081777941b94b6d4be8b9a7bd
Le Kha Hieu <lekhahieu03@gmail.com>
2026-02-08 17:01:46 +0700

    them nut on top

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import BackToTop from './components/common/BackToTop';
 import HomePage from './pages/HomePage';
 import WatchPage from './pages/WatchPage';
 
@@ -18,6 +19,7 @@ function App() {
                     </Routes>
                 </main>
                 <Footer />
+                <BackToTop />
             </div>
         </Router>
     );
